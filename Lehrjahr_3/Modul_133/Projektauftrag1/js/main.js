4b6453fb85ca9d60a43654ae9b6bd7954cf2d963
Severin Kaderli <severin.kaderli@gmail.com>
2015-10-15 20:57:01 +0200

    More updates to js and html code

--- Lehrjahr_3/Modul_133/Projektauftrag1/js/main.js
+++ Lehrjahr_3/Modul_133/Projektauftrag1/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	/**
 	 * This function validates the given string with the given expression
 	 *
-	 * @param {string} expression
+	 * @param {RegExp} expression
 	 * @param {string} string
 	 * @returns {boolean}
 	 */
@@ -12,20 +12,24 @@ $(document).ready(function() {
 		return exp.test(string);
 	}
 
-	/**
-	 * The regular expression to validate a password.
-	 */
-	var PASSWORD_EXPRESSION = "/^[a-zA-Z0-9]{9,}$/i";
+    /**
+     * The regular expression to validate a password.
+     *
+     * @type {RegExp}
+     */
+	var PASSWORD_EXPRESSION = /^[a-zA-Z0-9]{9,}$/i;
 
-	/**
-	 * The regular expression to validate an email address
-	 */
-	var EMAIL_EXPRESSION = "/^[a-zA-Z0-9-_\.]{3,}@{1}[a-zA-Z0-9-_\.]{3,}\.[a-z]{2,}$/i";
+    /**
+     * The regular expression to validate an email address
+     *
+     * @type {RegExp}
+     */
+	var EMAIL_EXPRESSION = /^[a-zA-Z0-9-_\.]{3,}@{1}[a-zA-Z0-9-_\.]{3,}\.[a-z]{2,}$/i;
 
     /**
      * Object which holds messages for the different errors
      */
-    var ERROR_MESSAGES = {
+    var ERROR_MESSAGE = {
         username: "Der Benutzername ist keine valide E-Mail Adresse",
         password: "Das Passwort entspricht nicht den Vorschriften!",
         comparePassword: "Die Passwörter stimmen nicht überein.",
@@ -38,29 +42,43 @@ $(document).ready(function() {
      */
     $('#loginForm').submit(function() {
 
-        //Array to hold the current error messages
         var errors = [];
 
         /**
          * Check if username is a valid email address using plain javascript
          */
-        if(!validate(EMAIL_EXPRESSION, "test")) {
-            errors.push(ERROR_MESSAGES.username);
+        if(!validate(EMAIL_EXPRESSION, document.getElementById("loginUsername").value)) {
+            errors.push(ERROR_MESSAGE.username);
         }
 
         /**
-         * Check if the password is valid using a jquery selector
+         * Check if the password is valid using a jQuery selector
          */
-        if(!validate(PASSWORD_EXPRESSION, "test")) {
-            errors.push(ERROR_MESSAGES.password);
+        if(!validate(PASSWORD_EXPRESSION, $('#loginPassword').val())) {
+            errors.push(ERROR_MESSAGE.password);
         }
 
-        //Display errors/input
-        console.log(errors);
+        /**
+         * Either display errors if there are any or show the input of the user
+         */
+        $("#loginError").html("");
+        $("#loginInput").html("");
+        var output = "";
+        if(errors.length > 0){
+            output += "<strong>Es sind Fehler aufgetreten:</strong><br>";
+            errors.forEach(function(value) {
+                output += value + "<br>";
+            });
+            $("#loginError").html(output);
+        } else {
+            output += "<strong>Ihre Eingaben:</strong><br>";
+            output += "Benutzername: " + $("#loginUsername").val() + "<br>";
+            output += "Passwort: ***************";
+            $("#loginInput").html(output);
+        }
 
         return false;
     });
-
 
     /**
      * Validate the register form
@@ -68,11 +86,33 @@ $(document).ready(function() {
     $('#registerForm').submit(function() {
 
         var errors = [];
-        //Check username
-        //Check password
+
+        /**
+         * Check if username is a valid email address using plain javascript
+         */
+        if(!validate(EMAIL_EXPRESSION, document.getElementById("username").value)) {
+            errors.push(ERROR_MESSAGE.username);
+        }
+
+        /**
+         * Check if the password is valid using a jQuery selector
+         */
+        if(!validate(PASSWORD_EXPRESSION, $('#password').val())) {
+            errors.push(ERROR_MESSAGE.password);
+        }
+
+
         //Compare password
         //Check options
         //Check checkbox
 
+    });
+
+
+    /**
+     * Display a user confirm when a user presses on one of the rest buttons
+     */
+    $(".btnReset").click(function() {
+       return confirm("Sind sie Sicher?");
     });
 });
