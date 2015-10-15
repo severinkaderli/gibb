16e970d2c5dc00076873ca5a43222b1972b1d08f
Severin Kaderli <severin.kaderli@gmail.com>
2015-10-15 20:12:43 +0200

    Updated Javascript code quite a bit

--- Lehrjahr_3/Modul_133/Projektauftrag1/js/main.js
+++ Lehrjahr_3/Modul_133/Projektauftrag1/js/main.js
@@ -1,18 +1,78 @@
 $(document).ready(function() {
 
 	/**
-	 * Validates a string with a regex pattern
-	 * 
-	 * @param regExp a RegExp-Object
-	 * @param string The string to validate
-	 * @return bool
+	 * This function validates the given string with the given expression
+	 *
+	 * @param {string} expression
+	 * @param {string} string
+	 * @returns {boolean}
 	 */
-	function validate(regExp, string) {
-		if(!regExp.test(string)) {
-			return false;
-		}
-		return true;
+	function validate(expression, string) {
+		var exp = new RegExp(expression);
+		return exp.test(string);
 	}
+
+	/**
+	 * The regular expression to validate a password.
+	 */
+	var PASSWORD_EXPRESSION = "/^[a-zA-Z0-9]{9,}$/i";
+
+	/**
+	 * The regular expression to validate an email address
+	 */
+	var EMAIL_EXPRESSION = "/^[a-zA-Z0-9-_\.]{3,}@{1}[a-zA-Z0-9-_\.]{3,}\.[a-z]{2,}$/i";
+
+    /**
+     * Object which holds messages for the different errors
+     */
+    var ERROR_MESSAGES = {
+        username: "Der Benutzername ist keine valide E-Mail Adresse",
+        password: "Das Passwort entspricht nicht den Vorschriften!",
+        comparePassword: "Die Passwörter stimmen nicht überein.",
+        options: "Mindest eine Option muss ausgewählt sein.",
+        checkbox: "Mindestens eine Checkbox muss angekreuzt sein."
+    };
+
+    /**
+     * Validate the login form
+     */
+    $('#loginForm').submit(function() {
+
+        //Array to hold the current error messages
+        var errors = [];
+
+        /**
+         * Check if username is a valid email address using plain javascript
+         */
+        if(!validate(EMAIL_EXPRESSION, "test")) {
+            errors.push(ERROR_MESSAGES.username);
+        }
+
+        /**
+         * Check if the password is valid using a jquery selector
+         */
+        if(!validate(PASSWORD_EXPRESSION, "test")) {
+            errors.push(ERROR_MESSAGES.password);
+        }
+    });
+
+
+    /**
+     * Validate the register form
+     */
+    $('#registerForm').submit(function() {
+
+        var errors = [];
+        //Check username
+        //Check password
+        //Compare password
+        //Check options
+        //Check checkbox
+
+    });
+};
+
+	/*
 
 	//Validate the form on submit
 	$('#loginForm').submit(function() {
@@ -31,7 +91,7 @@ $(document).ready(function() {
 
 		//Check if password is valid using a jquery selector ()[]{}?!$%&/=*+~,.;:<>-_
 		//TODO: Fix the regex
-		if(!validate(new RegExp(/^[a-zA-Z0-9]{9,}$/i), $('#password').val())) {
+		if(!validate(new RegExp(), $('#password').val())) {
 			errors.push('Das Passwort entspricht nicht den Vorschriften (Min. 9 Zeichen lang, min. 1 Grossbuchstabe, min. 1 Kleinbuchstabe und min. 1 Sonderzeichen)');
 		}
 
