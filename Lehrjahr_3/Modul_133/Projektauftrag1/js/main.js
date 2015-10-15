19526ad17427b42c632ba695fa6ef0bf275c0dc6
Severin Kaderli <severin.kaderli@gmail.com>
2015-10-15 20:15:37 +0200

    Removed unneccesary js code

--- Lehrjahr_3/Modul_133/Projektauftrag1/js/main.js
+++ Lehrjahr_3/Modul_133/Projektauftrag1/js/main.js
@@ -54,6 +54,11 @@ $(document).ready(function() {
         if(!validate(PASSWORD_EXPRESSION, "test")) {
             errors.push(ERROR_MESSAGES.password);
         }
+
+        //Display errors/input
+        console.log(errors);
+
+        return false;
     });
 
 
@@ -70,56 +75,4 @@ $(document).ready(function() {
         //Check checkbox
 
     });
-};
-
-	/*
-
-	//Validate the form on submit
-	$('#loginForm').submit(function() {
-		
-		//Array for error messages
-		var errors = [];
-		//TODO: Print messages out nicely
-		console.log('submitted');
-		
-		
-		//Check if username is a valid email address using a standard js selector
-		//Fix the regex
-		if(!validate(new RegExp(/^[a-zA-Z0-9-_\.]{3,}@{1}[a-zA-Z0-9-_\.]{3,}\.[a-z]{2,}$/i), document.getElementById('username').value)) {
-			errors.push('Der Benutzername muss eine gültige E-Mail Adresse sein!');
-		}
-
-		//Check if password is valid using a jquery selector ()[]{}?!$%&/=*+~,.;:<>-_
-		//TODO: Fix the regex
-		if(!validate(new RegExp(), $('#password').val())) {
-			errors.push('Das Passwort entspricht nicht den Vorschriften (Min. 9 Zeichen lang, min. 1 Grossbuchstabe, min. 1 Kleinbuchstabe und min. 1 Sonderzeichen)');
-		}
-
-		//Check if passwords match using plain javascript
-		if(document.getElementById('password').value != document.getElementById('password_confirmation').value) {
-			errors.push('Die Passwörter stimmen nicht überein');
-		}
-
-		//Check checkboxes using jQuery
-
-
-		//Either display all errors or alls inputs if valid
-		if(errors.length > 0) {
-			var errorText = "";
-			errors.forEach(function(element, index) {
-				errorText += errors[index] + "<br>";
-			});
-			$("#errors").html(errorText);
-			$("#errors").show();
-		} else {
-			console.log('good job');
-			$("#errors").hide();
-		}
-		return false;
-	});
-
-	//Message with reset button
-	$('#btnReset').click(function() {
-		return confirm('Möchten Sie wirklich alle Informationen zurücksetzen?');
-	});
 });
