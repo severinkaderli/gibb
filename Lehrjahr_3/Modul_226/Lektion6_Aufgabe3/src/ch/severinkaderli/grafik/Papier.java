bf225f70852d06d009e0ec0406dc62ee15cc66e6
Severin Kaderli <severin.kaderli@gmail.com>
2015-10-19 18:58:22 +0200

    Updated .gitignore

---
+++ Lehrjahr_3/Modul_226/Lektion6_Aufgabe3/src/ch/severinkaderli/grafik/Papier.java
@@ -0,0 +1,47 @@
+package ch.severinkaderli.grafik;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+/**
+ * Papier ist eine von JPanel abgeleitete Swing-Komponente.
+ * - Sie dient als "Zeichenblatt" für Zeichnungen.
+ * - Sie speichert die Zeichnung in einer Instanzvariablen.
+ * - Sie überschreibt die Methode paintComponent der Klasse JPanel
+ *   so, dass bei ihrem Aufruf die Zeichnung auf das Papier gezeichnet
+ *   wird.
+ * 
+ * @author Andres Scheidegger
+ */
+@SuppressWarnings("serial")
+public class Papier extends JPanel {
+	
+	/**
+	 * Die Zeichnung.
+	 */
+	private Zeichnung zeichnung;
+	
+	/**
+	 * Die Methode paintComponent zeichnet die Zeichnung auf das Papier.
+	 * Sie erhält dazu ein Graphics-Objekt, welches sie an die Zeichnung
+	 * weitergibt.
+	 * 
+	 * @param g  Graphics-Objekt, welches zum zeichnen verwendet werden soll.
+	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+	 */
+	@Override
+	public void paintComponent(final Graphics g) {
+    super.paintComponent(g);
+		if (zeichnung != null) zeichnung.zeichne(g);
+	}
+	
+	/**
+	 * Setter für die Instanzvariable zeichnung.
+	 * 
+	 * @param zeichnung Die zu zeichnende Zeichnung.
+	 */
+	public void setZeichnung(final Zeichnung zeichnung) {
+		this.zeichnung = zeichnung;
+	}
+}
