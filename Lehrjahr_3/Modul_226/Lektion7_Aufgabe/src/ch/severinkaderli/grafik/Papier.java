03002e47148a6dbf29b963a0ccf61e617af638d4
Severin Kaderli <severin.kaderli@gmail.com>
2015-10-20 15:14:24 +0200

    Added PapierKey- and PapierMouseListener

--- Lehrjahr_3/Modul_226/Lektion7_Aufgabe/src/ch/severinkaderli/grafik/Papier.java
+++ Lehrjahr_3/Modul_226/Lektion7_Aufgabe/src/ch/severinkaderli/grafik/Papier.java
@@ -1,10 +1,10 @@
 package ch.severinkaderli.grafik;
 
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
 
@@ -18,7 +18,7 @@ import javax.swing.JPanel;
  * @author Andres Scheidegger
  */
 @SuppressWarnings("serial")
-public class Papier extends JPanel implements MouseListener, KeyListener {
+public class Papier extends JPanel {
 
 	/**
 	 * Die Zeichnung.
@@ -37,20 +37,12 @@ public class Papier extends JPanel implements MouseListener, KeyListener {
 	private DrawingMode currentMode = DrawingMode.RECTANGLE;
 
 	public Papier() {
-		addMouseListener(this);
-		addKeyListener(this);
-		//This is needed for the KeyListener to work
+		addMouseListener(new PapierMouseListener());
+		addKeyListener(new PapierKeyListener());
+		// This is needed for the KeyListener to work
 		setFocusable(true);
 	}
 
-	/**
-	 * Die Methode paintComponent zeichnet die Zeichnung auf das Papier. Sie
-	 * erhält dazu ein Graphics-Objekt, welches sie an die Zeichnung weitergibt.
-	 * 
-	 * @param g
-	 *            Graphics-Objekt, welches zum zeichnen verwendet werden soll.
-	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
-	 */
 	@Override
 	public void paintComponent(final Graphics g) {
 		super.paintComponent(g);
@@ -58,110 +50,71 @@ public class Papier extends JPanel implements MouseListener, KeyListener {
 			zeichnung.zeichne(g);
 	}
 
-	/**
-	 * Setter für die Instanzvariable zeichnung.
-	 * 
-	 * @param zeichnung
-	 *            Die zu zeichnende Zeichnung.
-	 */
 	public void setZeichnung(final Zeichnung zeichnung) {
 		this.zeichnung = zeichnung;
 	}
 
 	/**
-	 * Mouse Events for Drawing the shapes
+	 * MouseListener for Drawing the shapes
 	 */
-	@Override
-	public void mousePressed(MouseEvent e) {
-		x = e.getX();
-		y = e.getY();
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) { 
-		
-		Figur figur;
-		switch(currentMode) {
-		case RECTANGLE:
-		default:
-			//TODO: Get this calculation right...
-			int tmp = 0;
-			int width = e.getX() - x;
-			int height = e.getY() - y;
-			
-			
-			/*if(x2 < x) {}
-				tmp = x2;
-				x = x2;
-				x2 = tmp;
-			if(y2 < y) {
-				tmp = y2;
-				y = y2;
-				y2 = tmp;
-			}*/
-			
-			figur = new Rechteck(x, y, width, height);
-			
-			break;
-		case LINE:
-			
-			figur = new Linie(x, y, e.getX(), e.getY());
-			
-			break;
-		case CIRCLE:
-			
-			//Calculate radius exactly
-			int dx = Math.abs(e.getX()) - x;
-			int dy = Math.abs(e.getY()) - y;
-			int radius = (int)Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-			figur = new Kreis(x, y, radius);
-			break;			
+	private class PapierMouseListener extends MouseAdapter {
+		public void mousePressed(MouseEvent e) {
+			x = e.getX();
+			y = e.getY();
 		}
-		
-		zeichnung.addFigur(figur);
-		repaint();
-	}
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-	}
+		public void mouseReleased(MouseEvent e) {
 
-	@Override
-	public void mouseEntered(MouseEvent e) {
-	}
+			Figur figur;
+			switch (currentMode) {
+			case RECTANGLE:
+			default:
+				// TODO: Get Calculation for negative values right
+				int width = e.getX() - x;
+				int height = e.getY() - y;
 
-	@Override
-	public void mouseExited(MouseEvent e) {
+				figur = new Rechteck(x, y, width, height);
+
+				break;
+			case LINE:
+
+				figur = new Linie(x, y, e.getX(), e.getY());
+
+				break;
+			case CIRCLE:
+
+				//Calculate radius with the pythogeroas theorem
+				int dx = Math.abs(e.getX()) - x;
+				int dy = Math.abs(e.getY()) - y;
+				int radius = (int) Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+				figur = new Kreis(x, y, radius);
+				break;
+			}
+
+			zeichnung.addFigur(figur);
+			repaint();
+		}
 	}
 
 	/**
-	 * Key events for switching drawing modes
-	 * 
-	 * r = Rectangles l = Lines k = Circles
+	 * KeyListener for switching current Drawing Mode
 	 */
-	@Override
-	public void keyPressed(KeyEvent e) {
-		switch (e.getKeyChar()) {
-		case 'r':
-			currentMode = DrawingMode.RECTANGLE;
-			break;
-			
-		case 'l':
-			currentMode = DrawingMode.LINE;
-			break;
-			
-		case 'k':
-			currentMode = DrawingMode.CIRCLE;
-			break;
+	private class PapierKeyListener extends KeyAdapter {
+		public void keyPressed(KeyEvent e) {
+			switch (e.getKeyChar()) {
+			case 'r':
+				currentMode = DrawingMode.RECTANGLE;
+				break;
+
+			case 'l':
+				currentMode = DrawingMode.LINE;
+				break;
+
+			case 'k':
+				currentMode = DrawingMode.CIRCLE;
+				break;
+			}
+			System.out.println(currentMode);
 		}
-		System.out.println(currentMode);
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
 	}
 }
