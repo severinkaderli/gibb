38c0a712bdadb03e82c3d1ffed420712ee65cad3
Severin Kaderli <severin.kaderli@gmail.com>
2015-10-27 08:36:12 +0100

    Updated Modul 133 files

--- Lehrjahr_3/Modul_133/Aufgabe6.1/index.php
+++ Lehrjahr_3/Modul_133/Aufgabe6.1/index.php
@@ -1,12 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Aufgabe 6.1 - Arrays</title>
+</head>
+<body>
+	<?php
 	/**
 	 * Aufgabe 1a
 	 */
+	echo "<h1>Aufgabe 1a - Array</h1>";
 	$weekdays = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"];
 	for($i = 0; $i < count($weekdays); $i++) {
 		echo $weekdays[$i] . "<br>";
 	}
-	echo "<br>";
 
 	/**
 	 * Aufgabe 1b
@@ -16,7 +22,11 @@
 	/**
 	 * Aufgabe 1c
 	 */
+	echo "<h1>Aufgabe 1b & 1c - Assoziativer Array</h1>";
 	foreach($weekdays2 as $shortDay => $weekday) {
 		echo $weekday . "<br>";
 	}
 ?>
+</body>
+</html>
+
--- Lehrjahr_3/Modul_133/Aufgabe6.1/weiterleitung.php
+++ Lehrjahr_3/Modul_133/Aufgabe6.1/weiterleitung.php
@@ -1,6 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Aufgabe 6.1 - Formular</title>
+</head>
+<body>
+	<?php
 	echo "Name: " . $_POST["txtBenutzername"] . "<br>";
 	echo "Vorname: " . $_POST["txtBenutzervorname"] . "<br>";
 	echo "Adresse: " . $_POST["txtBenutzeradresse"] . "<br>";
 	echo "Passwort: " . $_POST["txtPasswort"] . "<br>";
 ?>
+</body>
+</html>
+
