df020856b5a9a941a0af384ae8b228303e1571c4
Severin Kaderli <severin.kaderli@gmail.com>
2015-10-27 14:02:27 +0100

    Added JavaDoc

--- Lehrjahr_3/Modul_226/Lektion8_Aufgabe2/src/ch/severinkaderli/grafik/Zeichnung.java
+++ Lehrjahr_3/Modul_226/Lektion8_Aufgabe2/src/ch/severinkaderli/grafik/Zeichnung.java
@@ -6,26 +6,44 @@ import java.util.List;
 
 public class Zeichnung {
 
+		/**
+		 * Liste die alle Figuren der Zeichnung enthält.
+		 */
 		private List<Figur> shapes = new ArrayList<Figur>();
 		
-		
+		/**
+		 * Delegiert das Zeichnen an die einzelnen Figuren
+		 * 
+		 * @param g
+		 */
 		public void zeichne(Graphics g) {
 			this.shapes.forEach(shape -> shape.zeichne(g));
 		}
 		
+		/**
+		 * Fügt der Zeichnung eine neue Figur hinzu
+		 * 
+		 * @param figur Figur die hinzugefügt wird.
+		 */
 		public void addFigur(Figur figur) {
 			this.shapes.add(figur);
 		}
 		
+		/**
+		 * Entfernt eine Figur von der Zeichnung.
+		 * 
+		 * @param figur Figur, die entfertn wird.
+		 */
 		public void removeFigur(Figur figur) {
 			this.shapes.remove(figur);
 		}
 		
+		/**
+		 * @param figur
+		 * @param dx
+		 * @param dy
+		 */
 		public void moveFigur(Figur figur, int dx, int dy) {
 			figur.move(dx, dy);
-		}
-		
-		public void printElements() {
-			this.shapes.forEach(shape -> System.out.println(shape.toString()));
 		}
 }
