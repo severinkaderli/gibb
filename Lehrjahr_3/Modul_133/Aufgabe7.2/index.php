9bc1addc8b305510874f6b6005e059a0e8023bd6
Severin Kaderli <severin.kaderli@gmail.com>
2015-11-03 09:05:27 +0100

    Aufgabe7.2 hinzugefügt

---
+++ Lehrjahr_3/Modul_133/Aufgabe7.2/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Aufgabe 7.2</title>
+</head>
+<body>
+	<?php 
+		/**
+		 * 1) Stringlänge
+		 */
+		count("String");
+
+		/**
+		 * 2)
+		 */
+		rtrim("string ");
+
+		/**
+		 * 3)
+		 */
+		ltrim(" string");
+
+		/**
+		 * 4)
+		 */
+		str_replace(" ", "", "string string");
+
+		/**
+		 * 5)
+		 */
+		str_replace("@", "[at]", "test@email.com");
+		
+		/**
+		 * 6)
+		 */
+		strtoupper("benutzername");
+
+		/**
+		 * 7)
+		 */
+		//Regex
+
+		/**
+		 * 8)
+		 */
+		//Regex
+
+		/**
+		 * 9)
+		 */
+		//Regex
+
+		/**
+		 * 10)
+		 */
+		//Regex
+
+		/**
+		 * 11)
+		 */
+		str_replace("Herr", "Frau", "Herr und Frau Mann");
+
+		/**
+		 * 12)
+		 */
+		htmlspecialchars("string&;<>");
+
+		/**
+		 * 15)
+		 */
+		strcasecmp("password", "PASSWORD");
+
+		/**
+		 * 16)
+		 */
+		substr_count("string?and?and?", "?");
+
+		/**
+		 * 17)
+		 */
+		//Keine Lust
+
+		/**
+		 * 18)
+		 */
+		password_hash("password");
+	?>
+</body>
+</html>
