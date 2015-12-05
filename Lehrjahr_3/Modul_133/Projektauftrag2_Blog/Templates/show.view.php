dbaacce9a922af2a7787e9bce1f3317d4339abeb
Severin Kaderli <severin.kaderli@gmail.com>
2015-12-05 15:52:09 +0100

    Fixed root and base path

--- Lehrjahr_3/Modul_133/Projektauftrag2_Blog/Templates/show.view.php
+++ Lehrjahr_3/Modul_133/Projektauftrag2_Blog/Templates/show.view.php
@@ -1,67 +1,71 @@
-<?php 
-	
-	//Check if there's a post with this id
-	$postId = $_GET["id"];
-	if(!is_numeric($postId)) {
-		header("Location: index.php");
-		exit();
-	}
-	$post = Post::find($postId);
-	if(is_null($post)) {
-		header("Location: index.php");
-		exit();
-	}
+<?php
 
-	$postUser = User::find($post->fk_user_id);
+require_once(__ROOT__ . "Templates/Partials/header.php");
 
-	//Display the post
-	echo "<div class='post'>";
-		echo "<header class='post__header'>";
-			echo "<h1>$post->title<br><small>" . strftime(DATE_FORMAT, $post->timestamp) . " von $postUser->firstname $postUser->lastname</small></h1>";
-		echo "</header>";
 
-		echo "<div class='post__content'>";
-			echo "<p>$post->content</p>";
-		echo "</div>";
-	echo "</div>";
+//Check if there's a post with this id
+$postId = $_GET["id"];
+if (!is_numeric($postId)) {
+    header("Location: index.php");
+    exit();
+}
+$post = Post::find($postId);
+if (is_null($post)) {
+    header("Location: index.php");
+    exit();
+}
 
-	//Display comments
-	echo "<h3>Kommentare</h3>";
-	$comments = Comment::getByPostId($post->id);
-	if(is_null($comments)) {
-		echo "<p>Keine Kommentare vorhanden</p>";
-	} else {
+$postUser = User::find($post->fk_user_id);
 
-		foreach($comments as $comment) {
+//Display the post
+echo "<div class='post'>";
+echo "<header class='post__header'>";
+echo "<h1>$post->title<br><small>" . strftime(DATE_FORMAT, $post->timestamp) . " von $postUser->firstname $postUser->lastname</small></h1>";
+echo "</header>";
 
-			$user = User::find($comment->fk_user_id);
+echo "<div class='post__content'>";
+echo "<p>$post->content</p>";
+echo "</div>";
+echo "</div>";
 
-			echo "<div class='comment panel panel-default panel-body'>";
-				echo "<header class='comment__header'>";
-					echo "<p>Kommentar veröffentlicht von $user->firstname $user->lastname</p>"; 
-				echo "</header>";
-				echo "<div class='comment_content'>";
-					echo $comment->comment;
-				echo "</div>";
+//Display comments
+echo "<h3>Kommentare</h3>";
+$comments = Comment::getByPostId($post->id);
+if (is_null($comments)) {
+    echo "<p>Keine Kommentare vorhanden</p>";
+} else {
 
-			echo "</div>";
-			
-		}
-	}
+    foreach ($comments as $comment) {
 
-	//Only show add comment if logged in
-	echo "<h3>Kommentar verfassen</h3>";
-	if(User::auth()) {
-		
-		echo "<form method='POST' action=''>";
-			echo "<div class='form-group'>";
-				echo "<textarea id='comment' rows='5' name='comment' class='form-control'></textarea>";
-			echo "</div>";
-			echo "<div class='form-group'>";
-				echo "<input type='submit' class='btn btn-default' value='Kommentar absenden'>";
-			echo "</div>";
-		echo "</form>";
-	} else {
-		echo "<p>Sie müssen eingeloggt sein, um einen Kommentar zu verfassen!</p>";
-	}
-?>
+        $user = User::find($comment->fk_user_id);
+
+        echo "<div class='comment panel panel-default panel-body'>";
+        echo "<header class='comment__header'>";
+        echo "<p>Kommentar veröffentlicht von $user->firstname $user->lastname</p>";
+        echo "</header>";
+        echo "<div class='comment_content'>";
+        echo $comment->comment;
+        echo "</div>";
+
+        echo "</div>";
+
+    }
+}
+
+//Only show add comment if logged in
+echo "<h3>Kommentar verfassen</h3>";
+if (User::auth()) {
+
+    echo "<form method='POST' action=''>";
+    echo "<div class='form-group'>";
+    echo "<textarea id='comment' rows='5' name='comment' class='form-control'></textarea>";
+    echo "</div>";
+    echo "<div class='form-group'>";
+    echo "<input type='submit' class='btn btn-default' value='Kommentar absenden'>";
+    echo "</div>";
+    echo "</form>";
+} else {
+    echo "<p>Sie müssen eingeloggt sein, um einen Kommentar zu verfassen!</p>";
+}
+
+require_once(__ROOT__ . "Templates/Partials/footer.php");
--- Lehrjahr_3/Modul_133/Projektauftrag2_Blog/config.php
+++ Lehrjahr_3/Modul_133/Projektauftrag2_Blog/config.php
@@ -5,11 +5,6 @@
 session_start();
 
 /**
- * Path related settings
- */
-define("__ROOT__", __DIR__ . "/");
-
-/**
  * Site related settings
  */
 define("SITE_TITLE", "Blog");
@@ -18,14 +13,22 @@ define("SITE_AUTHOR", "Severin Kaderli");
 /**
  * Time and local related settings
  */
-define("LOCALE", "de_DE");    
+define("LOCALE", "de_DE");
 define("TIMEZONE", "Europe/Zurich");
 define("DATE_FORMAT", "%d. %B %Y");
+
+/**
+ * Path related settings
+ */
+define("__ROOT__", __DIR__ . "/");
+define("BASE_DIR", "http://" . dirname($_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF']));
+
 
 
 setlocale (LC_ALL, LOCALE);
 date_default_timezone_set(TIMEZONE);
 
+//Todo: Create an autoloader for classes
 require_once("./core/Database/DatabaseConnection.php");
 require_once("./core/Model/Model.php");
 require_once("./core/Model/Post.php");
